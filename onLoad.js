dc2f1a737d90205912e72e79d3e2e8a8617cb1e1
mbpd <mbpd010@gmail.com>
2015-08-22 10:44:03 +0100

    Added onLoad function

---
+++ onLoad.js
@@ -0,0 +1,11 @@
+var canvas, ctx;
+var width, height;
+
+window.onLoad = function() {
+    canvas =  document.getElementById("canvas");
+    ctx =  canvas.getContext("2d");
+
+    width = canvas.width;
+    height = canvas.height;
+
+}
