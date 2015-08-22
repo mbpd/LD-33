17172d8f3881f7fe950b1812e8cd55b17caf68a5
n42k/mys <n101010k@gmail.com>
2015-08-22 18:34:32 +0100

    Added a frame counter

--- html/onFrame.js
+++ html/onFrame.js
@@ -1,8 +1,12 @@
-function onFrame() {
+var currentFrame = 0;
+
+function onFrame()
+{
     canvas.width = canvas.width;
     ctx.fillStyle = "#FF0000";
     ctx.fillRect(0, 0, 800, 600);
     gameState.render();
     gameState.tick();
+    currentFrame++;
     window.requestAnimationFrame(onFrame);
 }
