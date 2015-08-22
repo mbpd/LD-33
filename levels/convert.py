14adfd6539de141c9b302957860b130d0961d9bd
n42k/mys <n101010k@gmail.com>
2015-08-22 13:14:05 +0100

    mend

--- levels/convert.py
+++ levels/convert.py
@@ -4,7 +4,7 @@ from sys import argv
 image = Image.open(argv[1])
 width, height = image.size
 
-level_index = argv[1].split('.')[0][5:]
+level_index = argv[1].split('/')[-1].split('.')[0][5:]
 
 javascript_string = "levels[" + level_index + "] = ["
 for y in range(height):
