c12b5c8fa989fd9637a6861f6644c53893fbe9d1
n42k/mys <n101010k@gmail.com>
2015-08-24 15:26:49 +0100

    Added a new computer image

--- html/objects/computer.js
+++ html/objects/computer.js
@@ -19,13 +19,13 @@ function Computer(x, y, message_id)
 
 Computer.prototype.draw = function()
 {
-    ctx.drawImage(images.computer, this.x + this.ox, this.y + this.oy);
+    ctx.drawImage(images.computer, this.x + this.ox, this.y + this.oy - 35);
 }
 
 Computer.prototype.canUse = function(x, y)
 {
     return x >= this.x + this.ox && x < this.x &&
-           y >= this.y + this.oy && y < this.y;
+           y >= this.y + this.oy - 35 && y < this.y - 35;
 }
 
 Computer.prototype.use = function()
