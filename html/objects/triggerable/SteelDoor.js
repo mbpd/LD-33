988dfb065eab805c6ac989fc13ae3ec6a3d42aea
n42k/mys <n101010k@gmail.com>
2015-08-24 14:29:45 +0100

    Fixed SteelDoor

--- html/objects/triggerable/SteelDoor.js
+++ html/objects/triggerable/SteelDoor.js
@@ -3,23 +3,37 @@ function SteelDoor(x, y)
     this.image = images.steeldoor_closed;
     this.x = x + TILE_SIZE;
     this.y = y + TILE_SIZE * 3;
+    
     this.triggered = false;
     this.ox = -TILE_SIZE;
     this.oy = -TILE_SIZE * 3;
 
+    this.centerX = this.x + this.ox/2;
+    this.centerY = this.y + this.oy/2;
+
+    this.drawX = this.x + this.ox;
+    this.drawY = this.y + this.oy - 64;
+
     this.collisionBox = [this.x + this.ox, this.y + this.oy, this.x, this.y];
+
+    this.realX = this.x;
+    this.realY = this.y;
 }
 
 SteelDoor.prototype.turnOn = function()
 {
     this.triggered = true;
     this.image = images.steeldoor_open;
+    this.x = this.realX - TILE_SIZE;
+    this.y = this.realY - 3 * TILE_SIZE;
 }
 
 SteelDoor.prototype.turnOff = function()
 {
     this.triggered = false;
     this.image = images.steeldoor_closed;
+    this.x = this.realX;
+    this.y = this.realY;
 }
 
 SteelDoor.prototype.toggle = function()
@@ -30,17 +44,17 @@ SteelDoor.prototype.toggle = function()
 
 SteelDoor.prototype.draw = function ()
 {
-   ctx.drawImage(this.image, this.x + this.ox, this.y + this.oy - 64); 
+   ctx.drawImage(this.image, this.drawX, this.drawY); 
 }
 
 SteelDoor.prototype.getCenterX = function()
 {
-    return this.x + this.ox/2;
+    return this.centerX;
 }
 
 SteelDoor.prototype.getCenterY = function()
 {
-    return this.y + this.oy/2;
+    return this.centerY;
 }
 
 SteelDoor.prototype.getCollisionBox = function()
